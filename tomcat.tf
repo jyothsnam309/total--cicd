43027552171bc11b5f45850c03cfe0a69b17c052
Your Name <jyothsnam309@gmail.com>
2022-09-23 10:24:44 +0530

    added

--- tomcat.tf
+++ tomcat.tf
@@ -23,8 +23,8 @@ resource "aws_security_group" "cicd-tomcat" {
 
  ingress {
     description = "connecting with enduser"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     security_groups = ["0.0.0.0/0"]
 
