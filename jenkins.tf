03e9ce1060b446a01acd89ad4ec43a358a000760
Your Name <jyothsnam309@gmail.com>
2022-09-23 10:19:23 +0530

    added

--- jenkins.tf
+++ jenkins.tf
@@ -55,17 +55,6 @@ resource "aws_instance" "cicd-jenkins" {
   #  key_name        = ${aws_key_pair.dev.id}
   key_name  = aws_key_pair.demo.id
 
-  user_data = <<-EOF
-#!/bin/bash
-wget -O /etc/yum.repos.d/jenkins.repo \
-   https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum update -y
-amazon-linux-extras install java-openjdk11
-yum install jenkins -y
-systemctl start jenkins
-systemctl enable jenkins
-              EOF
 
 
 
