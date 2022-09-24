abb5bcd581d5afaa40085c32e572abeefda0c2c5
Your Name <jyothsnam309@gmail.com>
2022-09-24 10:12:31 +0530

    added

---
+++ tomcat-userdata.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+sudo yum updata -y
+sudo amazon-linux-extras install java-openjdk11
+wget -o /opt/apache-tomcat-9.0.65-windows-x64.zip https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65-windows-x64.zip
